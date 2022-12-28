9d1f6e6d3a57deb5ada5fd084c6fe32a3ec7ef1b
PAMO <41010663+goxodn@users.noreply.github.com>
2022-12-29 01:27:04 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        input = input + "??!";
+        
+        System.out.println(input);
+        
+    }
+}
